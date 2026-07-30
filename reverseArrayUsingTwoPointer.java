3781c14eefb9e7a74d0eba93f339bb29d935cbe8
Sayak <sayakmodak2003@gmail.com>
2026-07-30 21:23:00 +0530

    reverse array using two pointer

---
+++ reverseArrayUsingTwoPointer.java
@@ -0,0 +1,20 @@
+public class reverseArrayUsingTwoPointer {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50 };
+        int n = arr.length;
+        int i = 0;
+        int j = n - 1;
+
+        while (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        for (int k = 0; k < n; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
