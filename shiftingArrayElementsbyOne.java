c9d57c7666822900d43ccf607eebc701efd02318
Sayak <sayakmodak2003@gmail.com>
2026-07-30 23:23:22 +0530

    added shiftingArrayElementsbyOne.java

---
+++ shiftingArrayElementsbyOne.java
@@ -0,0 +1,17 @@
+public class shiftingArrayElementsbyOne {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50, 60, 70 };
+        int n = arr.length;
+
+        // step1: last element k temp er moddhe rakha
+        int temp = arr[n - 1];
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
