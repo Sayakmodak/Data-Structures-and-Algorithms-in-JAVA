e801b34ada6b68c4353d5afca7b2ae5ba2f3c3c8
Sayak Modak <sayakmodak2003@gmail.com>
2026-08-03 08:53:54 +0530

    added Union of array

--- unionOfAnarray.java
+++ unionOfAnarray.java
@@ -1,29 +1,61 @@
-// pending
 import java.util.ArrayList;
 
 public class unionOfAnarray {
     public static void main(String[] args) {
-        int arr1[] = {1, 2, 3, 4, 5};
-        int arr2[] = {2, 5, 6, 7};
+       int arrA[] = {1,2,3,4,5};
+       int arrB[] = {2,5,6,7};
+        
+       ArrayList<Integer> sameElm = new ArrayList<>(); // [2,5]
+       ArrayList<Integer> arrAInter = new ArrayList<>();
+       ArrayList<Integer> arrBInter = new ArrayList<>();
 
-        ArrayList<Integer> sameArr = new ArrayList<>();
-        ArrayList<Integer> diffArr = new ArrayList<>();
-        // ArrayList<Integer> diffArr2 = new ArrayList<>();
 
-        for(int i=0; i<arr1.length; i++){
-            boolean found = false;
-            for(int j=0; j<arr2.length; j++){
-                if(arr1[i] == arr2[j]){
-                    found = true;
-                    break;
-                }
-            }
-
-            if(!found){
-                diffArr.add(arr1[i]);
+       for(int i=0; i<arrA.length; i++){
+        boolean found = false;
+        for(int j=0; j<arrB.length; j++){
+            if(arrA[i] == arrB[j]){
+                sameElm.add(arrA[i]);
+                found = true;
+                break;
             }
         }
+        if(!found){
+            arrAInter.add(arrA[i]);
+        }
+       }
 
-        System.out.println(diffArr);
-    }
-}
+       
+       /* for(int i=0; i<arrA.length; i++){
+        boolean found = false;
+        for(int j=0; j<arrB.length; j++){
+            if(arrA[i] == arrB[j]){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            arrAInter.add(arrA[i]);
+        }
+       } */
+
+       for(int i=0; i<arrB.length; i++){
+        boolean found = false;
+        
+        for(int j=0; j<arrA.length; j++){
+            if(arrB[i] == arrA[j]){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            arrBInter.add(arrB[i]);
+        }
+       }
+
+    //    System.out.println(arrAInter);
+    //    System.out.println(arrBInter);
+       
+       sameElm.addAll(arrAInter);
+       sameElm.addAll(arrBInter);
+       System.out.println(sameElm);
+    }}
